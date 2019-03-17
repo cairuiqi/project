447c869ed87e43b7ad0a9ae10d68882efdb13af3
cairuiqi <1461788863qq.com>
2019-03-17 16:37:00 +0800

    ok

---
+++ DianShang/app/src/main/java/com/bwei/crq/acitvity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bwei.crq.acitvity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwei.crq.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ DianShang/app/src/main/java/com/bwei/crq/fragment/ShoppingFragment.java
@@ -0,0 +1,9 @@
+package com.bwei.crq.fragment;
+
+/**
+ * @Auther: cairuiqi
+ * @Date: 2019/3/17 16:29:21
+ * @Description:
+ */
+public class ShoppingFragment {
+}
